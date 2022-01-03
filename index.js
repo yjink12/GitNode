482b7bcec6171473457161f8a96ce9c9185bb24f
yjin <yjin.k12@gmail.com>
2022-01-03 23:39:26 +0900

    DB Connect

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const db_config = require(__dirname + '/config/db.js');
+const conn = db_config.init();
+const bodyParser = require('body-parser');
+
+db_config.connect(conn);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
 
 app.get('/', (req, res) => {
-  res.send('testetest!!');
-  res.send('merge test~~~~~~~!!!!!');
-})
+  res.send('Root');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get('/member', (req, res) => {
+  let sql = 'SELECT * FROM member';
+  conn.query(sql, function(err, rows, fields){
+    if(err) console.log('query fail' + err);
+    else res.render('mem.ejs', {list : rows});
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Example app listening at`);
+});
